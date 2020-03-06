4170fd65043e53796d2d88f7b3f7926471da08eb
ysqi <devysq@gmail.com>
2020-03-06 17:04:34 +0800

    创建账户1

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 App = {
-
     init: () => {
 
         //按钮点击事件
         $(".menu .item").on("click", (e) => {
             //切换不同的 Tab
             App.switchWay(e)
+        })
+        $("[name='createWallet']").on("click",(e)=>{
+            App.createWallet()
         })
     },
     switchWay: (e) => {
@@ -17,6 +19,14 @@ App = {
             .attr("disabled", false)
         item.addClass("active").attr("disabled", true)
     },
+    // 实时创建一个钱包账户
+    createWallet:()=>{
+        let wallet=ethers.Wallet.createRandom();
+        $(".wallet").show();
+        $("#privateKey").text(wallet.privateKey);
+        $("#publicKey").text(wallet.signingKey.publicKey);
+        $("#address").text(wallet.address);
+    }
 }
 
 $(document).ready(() => {
