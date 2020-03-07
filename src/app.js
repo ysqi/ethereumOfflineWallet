4200923aeca17fbc0198d19cdfdb9f12006ed3f1
ysqi <devysq@gmail.com>
2020-03-07 10:19:47 +0800

    5.一键创建账户

--- src/app.js
+++ src/app.js
@@ -7,6 +7,10 @@ App = {
             //切换不同的 Tab
             App.switchWay(e)
         })
+        $("[name='createWallet']").on("click",(e)=>{
+            // 创建钱包
+            App.createWallet(e);
+        })
     },
     switchWay: (e) => {
         let item = $(e.currentTarget)
@@ -17,6 +21,16 @@ App = {
             .attr("disabled", false)
         item.addClass("active").attr("disabled", true)
     },
+    // 一键创建钱包账户
+    createWallet:(e)=>{
+        // 随机钱包
+        let wallet = ethers.Wallet.createRandom();
+
+        $("#privateKey").text(wallet.signingKey.privateKey);
+        $("#publicKey").text(wallet.signingKey.publicKey);
+        $("#address").text(wallet.signingKey.address);
+        $(".wallet").show();
+    }
 }
 
 $(document).ready(() => {
