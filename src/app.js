17707d56fb130c36923b0209bb23814551c45650
ysqi <devysq@gmail.com>
2020-03-07 14:13:53 +0800

    私钥开关

--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
 App = {
 
+    wallet: null,//当前创建的钱包账户
+    config: {
+        showPK: false,
+    },
     init: () => {
 
         //按钮点击事件
@@ -7,9 +11,13 @@ App = {
             //切换不同的 Tab
             App.switchWay(e)
         })
-        $("[name='createWallet']").on("click",(e)=>{
+        $("[name='createWallet']").on("click", (e) => {
             // 创建钱包
             App.createWallet(e);
+        })
+        $("#safeShowSwitch").on("click", (e) => {
+            App.config.showPK = !App.config.showPK;
+            App.switchPK();
         })
     },
     switchWay: (e) => {
@@ -22,14 +30,29 @@ App = {
         item.addClass("active").attr("disabled", true)
     },
     // 一键创建钱包账户
-    createWallet:(e)=>{
+    createWallet: (e) => {
         // 随机钱包
         let wallet = ethers.Wallet.createRandom();
-
-        $("#privateKey").text(wallet.signingKey.privateKey);
+        App.wallet = wallet;
+        App.setPK();
         $("#publicKey").text(wallet.signingKey.publicKey);
         $("#address").text(wallet.signingKey.address);
         $(".wallet").show();
+    },
+    setPK: () => {
+        if (App.config.showPK) {
+            $("#privateKey").text(App.wallet.signingKey.privateKey);
+        } else {
+            //0x+ 64个 * (私钥 32 字节,64 个十六进制的字符)
+            $("#privateKey").text("*".repeat(64 + 2));
+        }
+    },
+    // 切换显示私钥
+    switchPK: () => {
+        let btn = $("#safeShowSwitch")
+        btn.find(".closeEye").toggle();//隐藏或显示
+        btn.find(".openEye").toggle();
+        App.setPK();
     }
 }
 
